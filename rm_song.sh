169f4d2ff3fa11a88a13fa934e89ac173a86aa24
Benjamin Horvat <budikutija@gmail.com>
2017-06-01 02:09:13 +0200

    scripts added

---
+++ rm_song.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+VAR="48"
+rm src/song/"$VAR".ily
+rm src/chords/"$VAR"_c.ily
+rm src/lyrics/"$VAR"_l.ily
+rm src/notes/soprano/"$VAR"_s.ily
+rm src/notes/alto/"$VAR"_a.ily
+rm src/notes/tenor/"$VAR"_t.ily
+rm src/notes/bass/"$VAR"_b.ily
+rm src/score/choir/"$VAR"_choir.ly
+rm src/score/lead/"$VAR"_lead.ly
---
+++ song_to_hidden.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+VAR="48"
+mv src/song/"$VAR".ily src/song/."$VAR".ily
+mv src/chords/"$VAR"_c.ily src/chords/."$VAR"_c.ily
+mv src/lyrics/"$VAR"_l.ily src/lyrics/."$VAR"_l.ily
+mv src/notes/soprano/"$VAR"_s.ily src/notes/soprano/."$VAR"_s.ily
+mv src/notes/alto/"$VAR"_a.ily src/notes/alto/."$VAR"_a.ily
+mv src/notes/tenor/"$VAR"_t.ily src/notes/tenor/."$VAR"_t.ily
+mv src/notes/bass/"$VAR"_b.ily src/notes/bass/."$VAR"_b.ily
+mv src/score/choir/"$VAR"_choir.ly src/score/choir/."$VAR"_choir.ly
+mv src/score/lead/"$VAR"_lead.ly src/score/lead/."$VAR"_lead.ly
